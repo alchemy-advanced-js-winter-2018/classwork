ccb71b45f71c18b49d06278f67bd28d63ff8d321
martypdx <marty@martypdx.com>
2018-02-12 09:49:18 -0800

    add "stranger" default when no name provided

--- class01_node-testing-arrays/greeting-library/test/greeting.test.js
+++ class01_node-testing-arrays/greeting-library/test/greeting.test.js
@@ -1,10 +1,15 @@
 const assert = require('assert');
 
-function greet(name) {
+function greet(name = 'stranger') {
     return `hello ${name}`;
 }
 
 it('greets with hello <name>', () => {
     const greeting = greet('timmy');
     assert.equal(greeting, 'hello timmy');
+});
+
+it('greets with hello stranger when no name provided', () => {
+    const greeting = greet();
+    assert.equal(greeting, 'hello stranger');
 });
